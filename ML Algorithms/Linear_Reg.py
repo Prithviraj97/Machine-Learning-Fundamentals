912c47ef3c318a17edab1a02cf1e9aa9ad82b613
Prithviraj97 <prithvirajsingh358@gmail.com>
2025-02-08 18:29:14 -0600

    ML Algorithms implementation

---
+++ ML Algorithms/Linear_Reg.py
@@ -0,0 +1,6 @@
+'''
+Our goal here is to first implement a simple linear regression model without using any libraries.
+Goal - implement the Linear Regression Algorithm.
+       use the linear regression model to predict the price of a house given the area of the house.
+       Visualize the data and the model.
+'''
