566545e846be9c91258932c494b23b7042a20f86
Prithviraj97 <prithvirajsingh358@gmail.com>
2025-02-10 18:30:19 -0600

    trying to implement multiple regression

---
+++ ML Algorithms/MultiRegression.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#let's write code for multiple linear regression model not in class form or function form.
+#initialize the feature data
+x1 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+x2 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+x3 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
