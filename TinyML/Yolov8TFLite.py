5a304c5a19f703a423c09ab1958838f1fba9223e
Prithvi Raj Singh <prithvirajsingh358@gmail.com>
2025-03-26 12:14:33 -0500

    still attempting conversion

--- TinyML/Yolov8TFLite.py
+++ TinyML/Yolov8TFLite.py
@@ -1,4 +1,3 @@
 from ultralytics import YOLO
-model = YOLO('C:\\Users\\Admin\\Machine Learning Fundamentals\\yolov5s.pt')
-
-model.export(format = 'tflite', int8=True)
+model = YOLO('yolov8n.pt')
+model.export(format='tflite', int8=True)
